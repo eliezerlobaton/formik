c678274e6f9a379bc69999cd56b7b57a088be743
Eliezer Lobaton <eliezerlobaton39@gmail.com>
2022-09-25 13:35:33 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,44 @@
-import { useFormik } from 'formik'
-function App() {
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      lastname: '',
-      email: '',
-    },
-    onSubmit: values => console.log(values),
-  })
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+
+const validate = (values) => {
+  const errors = {}
+  if (!values.name) { errors.name = 'requerido' } else if (values.name.length < 5) {
+    errors.name = 'El nombre es muy corto'
+  }
+
+  if (!values.lastname) { errors.lastname = 'requerido' } else if (values.lastname.length < 5) {
+    errors.lastname = 'El apelido es muy corto'
+  }
+
+  if (!values.email) { errors.email = 'requerido' } else if (values.email.length < 5) {
+    errors.email = 'El email es muy corto'
+  }
+
+  return errors
+}
+const App = () => {
+
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label>Nombre</label>
-      <input
-        name='name'
-        type='text'
-        onChange={formik.handleChange}
-        value={formik.values.name}
-      />
-      <label>Apellido</label>
-      <input
-        name='lastname'
-        type='text'
-        onChange={formik.handleChange}
-        value={formik.values.lastname}
-      />
-      <label>Email</label>
-      <input
-        name='email'
-        type='text'
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      <button type='submit'>Enviar</button>
-    </form>
+    <Formik
+      initialValues={ { name: '', lastname: '', email: '', } }
+      validate={ validate }
+      onSubmit={ values => console.log(values) }
+    >
+      <Form>
+        <label>Nombre</label>
+        <Field name='name' type='text' />
+        <ErrorMessage name='name' />
+        <br />
+        <label>Apellido</label>
+        <Field name='lastname' type='text' />
+        <ErrorMessage name='lastname' />
+        <br />
+        <label>Email</label>
+        <Field name='email' type='email' />
+        <ErrorMessage name='email' />
+        <button type='submit'>Enviar</button>
+      </Form>
+    </Formik>
   )
 }
 
